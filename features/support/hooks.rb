eace0a0bb5d69236100af401e7ec911b64bebb0a
Joseph Wilk <joe@josephwilk.net>
2010-08-30 22:53:36 +0100

    Adding new fixture setup to handle api and username changes

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,14 @@
+def setup_fixtures(connection)
+  doc = {:username => 'josephwilk', :email => 'j@o.e', :api_key => '123'}
+  connection.db('cuke_max')['mm_users'].insert(doc)
+  docs = [{:project_id => 'cuke_internal_tests', :user_id => 'josephwilk'},
+          {:project_id => 'build_1', :user_id => 'josephwilk'},
+          {:project_id => 'build_2', :user_id => 'josephwilk'}]
+  docs.each { |doc| connection.db('cuke_max')['project'].insert(doc) }
+end
+
 Before do
   m = Mongo::Connection.new('localhost',Mongo::Connection::DEFAULT_PORT)
   m.drop_database('cuke_max')
+  setup_fixtures(m)
 end
