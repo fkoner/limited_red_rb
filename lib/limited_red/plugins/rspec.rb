54213c50389a5612590cf91a3713e16725226ed7
Joseph Wilk <joe@josephwilk.net>
2012-01-06 18:08:46 +0000

    Playing around with the logging for rspec

--- lib/limited_red/plugins/rspec.rb
+++ lib/limited_red/plugins/rspec.rb
@@ -1,13 +1,34 @@
 RSpec.configure do |config|
+  CLIENT = LimitedRed::Client.new(LimitedRed::Config.load_and_validate_config)
+  BUILD_ID = Time.now
+
+  fails = []
+  passes = []
+  
   config.after(:each) do
     metadata = example.metadata
     full_description = example.metadata[:full_description]
     file, line = *(example.metadata[:example_group_block].source_location)
+
+    json = {:type => 'rspec'
+            :file =>  file, 
+            :line => line, 
+            :uri => full_description}.to_json
   
     if example.exception #Fail
-      puts "FAIL", file, line
+      fails << file
+      CLIENT.log_result(BUILD_ID, :result => json)
+      puts "Client FAIL", file, line
     else # Pass
+      passes << file
+      CLIENT.log_result(BUILD_ID, :result => json)
       puts "PASS", file, line
     end
   end
+  
+  config.after(:all) do
+    @client.log_build(BUILD_ID, {:fails => failing_files,
+                                 :passes => passing_files})
+  end
+
 end
