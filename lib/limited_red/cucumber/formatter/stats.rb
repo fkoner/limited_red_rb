7ab4397f08b2a0d4b9c1c43ac0669e57ed9180c6
josephwilk <joe@josephwilk.net>
2012-09-23 10:06:54 -0600

    Stop fake web getting in the way

--- lib/limited_red/cucumber/formatter/stats.rb
+++ lib/limited_red/cucumber/formatter/stats.rb
@@ -19,7 +19,10 @@ module LimitedRed
         def after_feature(feature)
           if supports_feature_hash?
             json = feature_hash.to_json
-            @client.log_result(@build_id, :result => json, :uri => feature_hash['uri'])
+            
+            without_fakeweb do
+              @client.log_result(@build_id, :result => json, :uri => feature_hash['uri'])
+            end
           else
             puts "[limited_red]:Error: Having trouble working with your Gherkin version. Is it upto date? Report this to joe@josephwilk.net, its his fault.", ""
           end
@@ -30,8 +33,10 @@ module LimitedRed
         end
 
         def print_summary
+          without_fakeweb do
            @client.log_build(@build_id, {:fails => failing_files, 
                                          :passes => passing_files})
+          end
                                                    
           ThreadPool.wait_for_all_threads_to_finish
         end
@@ -58,6 +63,12 @@ module LimitedRed
       
         def gherkin_formatter
           @gf
+        end
+        
+        def without_fakeweb
+          FakeWeb.allow_net_connect = true if defined?(FakeWeb)
+          yield
+          FakeWeb.allow_net_connect = false if defined?(FakeWeb)
         end
       
       end
